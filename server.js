9b3871d55e8d16492e44af9c54dfed73a35a48f2
vishnu0179 <vjvj140@gmail.com>
2019-10-10 03:08:27 +0530

    test1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/',(req,res)=>{
+app.get('/vishnu',(req,res)=>{
     res.send('Server started');
 })
 
